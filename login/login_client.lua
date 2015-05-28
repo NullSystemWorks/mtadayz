623aa7c6c872ff3d3e2dae3e7ce4c0f54dfbb2dc
mtadayz-dev <rysttimes@gmail.com>
2015-05-28 11:33:59 +0200

    Fixed issue #12 (https://bitbucket.org/mtadayz/mta-dayz/issue/12/wrong-dayz-version-showing-on-login)

--- login/login_client.lua
+++ login/login_client.lua
@@ -1,6 +1,6 @@
 resourceRoot = getResourceRootElement( getThisResource( ) )
 localPlayer = getLocalPlayer()
-versionstring = "MTA:DayZ\nwww.vavegames.net\n\nVersion: 1.0.1b\nLast Update: 13.04.14"
+versionstring = "MTA:DayZ\nVersion: 0.8b\nLast Update: 28/05/15"
 infoTable = {}
 Login_Edit = {}
 marwinButtons = {}
