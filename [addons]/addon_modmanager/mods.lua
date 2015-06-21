931206fd5801f72c21c1ab9e467c3a97dcae60ed
Jack Johnson <jj.j11@hotmail.co.uk>
2015-06-21 22:00:37 +0100

    Mod Manager
    - Added Mod Manager system (basic, but it'll do for the players).
    - Added a "Jeep" mod for the Sandking (as an example)

---
+++ [addons]/addon_modmanager/mods.lua
@@ -0,0 +1,6 @@
+--DON'T FORGET TO ADD DFF_FILE, TXD_FILE AND COL_FILE TO THE META.XML!
+--IF there's no col file, leave it as ""
+mods = {
+	--{MODEL_ID,DFF_FILE,TXD_FILE,COL_FILE}
+	{395,"jeep.dff","jeep.txd",""} --Jeep mod, replacing SandKing (1B0Y)
+}
