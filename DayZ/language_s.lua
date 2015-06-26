73f5057b98bcb5a34a9b93ad97d5bbb7581b36b7
CiBeR96 <keevgross@gmail.com>
2015-06-26 12:41:38 -0300

    Introducing Multi Language Support

---
+++ DayZ/language_s.lua
@@ -0,0 +1,267 @@
+--[[
+
+	Name: Server-Side Language Tables
+	Version: 0.1
+	Copyright: DayZ Gamemode. All rights reserved to Developers
+	Current Devs: Lawliet, CiBeR, Jboy
+	
+]]--
+
+--[[ Defining Primary Language Tables ]]--
+languageItemTable = {}
+languageTextTable = {}
+------------------------------------------
+
+--[[ Defining Language Item Tables ]]--
+--English
+languageItemTable.en_EN = {
+	["Weapons"] = {
+		"M4",
+		"AK-47",
+		"Lee Enfield",
+		"CZ 550",
+		"MP5A5",
+		"PDW",
+		"SPAZ-12 Combat Shotgun",
+		"Sawn-Off Shotgun",
+		"Winchester 1866",
+		"M9 SD",
+		"M1911",
+		"Blaze 95 Double Rifle",
+		"Remington 870",
+		"Sporter 22",
+		"Revolver",
+		"Bizon PP-19",
+		"FN FAL",
+		"G36C",
+		"Sa58V CCO",
+		"DMR",
+		"SVD Dragunov",
+		"SKS",
+		"Crossbow",
+		"Desert Eagle",
+		"Binoculars",
+		"Tear Gas",
+		"Grenade",
+		"Satchel",
+		"Baseball Bat",
+		"Shovel",
+		"Golf Club",
+		"Hunting Knife",
+		"Hatchet"
+	},
+	
+	["Ammo"] = {
+		"5.56x45mm Cartridge",
+		"5.45x39mm Cartridge",
+		"9x18mm Cartridge",
+		"9x19mm Cartridge",
+		"9x19mm SD Cartridge",
+		"9.3x62mm Cartridge",
+		".45 ACP Cartridge",
+		"Gauge 12 Pellet",
+		"2Rnd. Slug",
+		"1866 Slug",
+		".303 British Cartridge",
+		"Bolt",
+		
+	},
+	
+	["Food/Drinks"] = {
+		"Baked Beans",
+		"Pasta",
+		"Sardines",
+		"Frank & Beans",
+		"Can (Corn)",
+		"Can (Peas)",
+		"Can (Pork)",
+		"Can (Stew)",
+		"Can (Ravioli)",
+		"Can (Fruit)",
+		"Can (Chowder)",
+		"MRE",
+		"Pistachios",
+		"Trail Mix",
+		"Water Bottle",
+		"Soda Can (Pepsi)",
+		"Soda Can (Cola)",
+		"Soda Can (Mountain Dew)",
+		"Can (Milk)",
+		"Raw Meat",
+		"Cooked Meat",
+	},
+	
+	["Backpacks"] = {
+		"Assault Pack (ACU)",
+		"Czech Vest Pouch",
+		"ALICE Pack",
+		"Survival ACU",
+		"British Assault Pack",
+		"Backpack (Coyote)",
+		"Czech Backpack",
+	},
+	
+	["Toolbelt"] = {
+		"Box of Matches",
+		"IR Goggles",
+		"NV Goggles",
+		"GPS",
+		"Map",
+		"Toolbox",
+		"Watch",
+		"Radio Device",
+		"Compass"
+	},
+	
+	["Medical Items"] = {
+		"Bandage",
+		"Morphine",
+		"Heat Pack",
+		"Blood Bag",
+		"Painkiller",
+	},
+	
+	["Car Parts"] = {
+		"Tire",
+		"Engine",
+		"Tank Parts",
+	},
+	
+	["Clothes"] = {
+		"Survivor Clothing",
+		"Civilian Clothing",
+		"Camouflage Clothing",
+		"Ghillie Suit",
+	},
+	
+	["Blueprints"] = {
+		"M4 Blueprint",
+		"CZ 550 Blueprint",
+		"Winchester '66 Blueprint",
+		"SPAZ-12 C. Shtgn. Blueprint",
+		"Sawn-Off Shtgn. Blueprint",
+		"AK-47 Blueprint",
+		"Lee Enfield Blueprint",
+		"Sporter 22 Blueprint",
+		"Mosin 9130 Blueprint",
+		"Crossbow Blueprint",
+		"SKS Blueprint",
+		"Blaze 95 D. R. Blueprint",
+		"Remington 870 Blueprint",
+		"FN FAL Blueprint",
+		"G36C Blueprint",
+		"Sa58V Blueprint",
+		"SVD Blueprint",
+		"DMR Blueprint",
+		"M1911 Blueprint",
+		"M9 SD Blueprint",
+		"PDW Blueprint",
+		"G17 Blueprint",
+		"MP5A5 Blueprint",
+		"Bizon PP-19 Blueprint",
+		"Revolver Blueprint",
+		"Desert Eagle Blueprint",
+		"Hunting Knife Blueprint",
+		"Hatchet Blueprint",
+		"Baseball Bat Blueprint",
+		"Shovel Blueprint",
+		"Golf Club Blueprint",
+		"Crowbar Blueprint",
+		"Parachute Blueprint",
+		"Grenade Blueprint",
+		"Binoculars Blueprint",
+		".45 ACP Cartridge Blueprint",
+		"9x19mm SD Cartridge Blueprint",
+		"9x19mm Cartridge Blueprint",
+		"9x18mm Cartridge Blueprint",
+		"5.45x39mm Cartridge Blueprint",
+		"5.56x45mm Cartridge Blueprint",
+		"1866 Slug Blueprint",
+		"2Rnd. Slug Blueprint",
+		"12 Gauge Pellet Blueprint",
+		"9.3x62mm Cartridge Blueprint",
+		".303 British Cartridge Blueprint",
+		"Bolt Blueprint",
+		"Wire Fence Blueprint",
+		"Tent Blueprint",
+		"Camouflage Clthng. Blueprint",
+		"Survivor Clthng. Blueprint",
+		"Civilian Clthng. Blueprint",
+		"Ghillie Suit Blueprint",
+		"Road Flare Blueprint",
+		"Toolbox Blueprint",
+		"Radio Device Blueprint",
+		"IR Goggles Blueprint",
+		"NV Goggles Blueprint",
+	},
+	
+	["Blueprint Parts"] = {
+		"Gun Barrel",
+		"Short Gun Barrel",
+		"Gun Stock",
+		"Thread",
+		"Cloth",
+		"Gun Powder",
+		"Mechanical Supplies",
+		"Cables",
+		"Nails",
+		"Sheet",
+		"Barbed Wire",
+		"Duct Tape",
+		"Glue",
+		"Drugs",
+		"Bandaid",
+		"Vitamins",
+		"Tissue",
+		"Small Box",
+		"String",
+		"Needle",
+		"Microchips",
+		"Optics",
+		"Sharp Metal",
+		"Handle",
+		"Wooden Stick",
+		"Hand Saw",
+		"Metal Plate",
+		"Metallic Stick",
+		"Small Casing",
+	},
+	
+	["Other"] = {
+		"Wood Pile",
+		"Empty Gas Canister",
+		"Full Gas Canister",
+		"Road Flare",	
+		"Wire Fence",
+		"Tent"
+	}
+}
+--Spanish
+languageItemTable.es_ES = {
+
+}
+--Arabic
+languageItemTable.ar_AR = {
+
+}
+--German
+languageItemTable.de_DE = {
+
+}
+--Portuguese
+languageItemTable.pt_PT = {
+
+}
+--French
+languageItemTable.fr_FR = {
+
+}
+--Russian
+languageItemTable.ru_RU = {
+
+}
+--Polish
+languageItemTable.pl_PL = {
+
+}
+---------------------------------------
