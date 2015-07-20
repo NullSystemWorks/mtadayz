5e3806b34c4efd0218ff8b12a6d59ca8aa9456f0
CiBeR96 <keevgross@gmail.com>
2015-07-20 13:12:33 -0300

    [CiBeR] Fixed White Texture bug Modmanager

--- [addons]/addon_modmanager/manager.lua
+++ [addons]/addon_modmanager/manager.lua
@@ -8,20 +8,19 @@ function onStart()
 			txd = v[3]
 			col = v[4]
 			
-			if (dff and dff ~= "") then
-				dff = engineLoadDFF("mods/"..dff)
-				engineReplaceModel(dff,v[1])
-			end
-			
 			if (txd and txd ~= "") then
 				txd = engineLoadTXD("mods/"..txd)
 				engineImportTXD(txd,v[1])
 			end
-			
 			if (col and col ~= "") then
 				col = engineLoadCOL("mods/"..col)
 				engineReplaceCOL(col,v[1])
 			end
+			if (dff and dff ~= "") then
+				dff = engineLoadDFF("mods/"..dff)
+				engineReplaceModel(dff,v[1])
+			end
+
 			
 			--Output what mod we just loaded
 			outputConsole("Mod "..v[2].." loaded.")
