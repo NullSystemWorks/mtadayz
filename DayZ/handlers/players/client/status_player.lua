9a4d50a2936136c036b0da32f7b2866997bb1bb5
mtadayz-dev <rysttimes@gmail.com>
2015-08-07 15:37:19 +0200

    [Debug]

--- DayZ/handlers/players/client/status_player.lua
+++ DayZ/handlers/players/client/status_player.lua
@@ -119,8 +119,10 @@ setTimer(setPlayerCold,40000,0)
 function isPlayerInBuilding(x,y,z)
 	if isInBuilding(x,y,z) then
 		triggerServerEvent("onPlayerChangeStatus",source,"isInBuilding",true)
+		outputChatBox("Player :"..getPlayerName(source).." is in a building",root)
 	else
 		triggerServerEvent("onPlayerChangeStatus",source,"isInBuilding",false)
+		outputChatBox("Player :"..getPlayerName(source).." is not in a building",root)
 	end
 end
 addEvent("isPlayerInBuilding",true)
