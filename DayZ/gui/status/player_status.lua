885d1448fcce093b3e1eb1919bf90a9ad35ef86a
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2015-08-27 21:51:08 -0300

    Continued commit 7c18e8a (temperature fix)

--- DayZ/gui/status/player_status.lua
+++ DayZ/gui/status/player_status.lua
@@ -71,8 +71,8 @@ function updateStatusIcons()
 		elseif temperature >= 37 and temperature <= 41 then
 			r,g,b = 0,255,0
 			t_number = 3
-		elseif temperature <= 41 then
-			number = 4
+		elseif temperature >= 41 then
+			t_number = 4
 			r,g,b = 255,0,0
 		end
 		if value > 215 then
