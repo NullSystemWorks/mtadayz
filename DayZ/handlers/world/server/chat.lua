bb6ec52e4adc8d2960b5df612cbe014126434fcc
XyrusPL <arcaro@wp.pl>
2017-02-12 20:07:54 +0100

    Update chat.lua
    Anti-advertising

--- DayZ/handlers/world/server/chat.lua
+++ DayZ/handlers/world/server/chat.lua
@@ -5,6 +5,12 @@ function broadcast(text, type, channel)
 		outputChat("#ff0000You are Muted!", source, "System", type, 4)
 		return
 	end
+	if gameplayVariables["noadvert"] == true then
+	local Chunks = {(text:gsub("%s+", "")):match("(%d+)%.(%d+)%.(%d+)%.(%d+)")}
+    	if (#Chunks == 4) then
+     	banPlayer ( source, false, false, true, nil, "Don't advertise other servers, you got an 1-day ban for it now.", 86400 )
+	end
+   	end
 	if channel == 1 then
 		local theTime = getRealTime()
 		local hour = theTime.hour
