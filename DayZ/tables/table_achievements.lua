c7725093a11483f3ae709c33df1e36e6288d4e7a
neves768 <chris_oboy@hotmail.com>
2017-02-26 21:27:25 -0300

    Updated table indexing type

--- DayZ/tables/table_achievements.lua
+++ DayZ/tables/table_achievements.lua
@@ -10,16 +10,14 @@
 
 pathToImg = "gui/achievements/icons/"
 achievements = {
-	[1] = {
-		["identification"] = "kill50p",
+	["kill50p"] = {
 		["name"] = "Killing 50 players",
 		["description"] = "Kill 50 players to win this achievement and some items",
 		["items"] = {{"Flashlight",1},{"Bandage",3}},
 		["conditions"] = {{"murders","greater",10},{"zombieskilled","less",10}},
 		["image"] = "achiev1.png"
 	},
-	[2] = {
-		["identification"] = "kill50z",
+	["kill50z"] = {
 		["name"] = "Killing 50 zombies",
 		["description"] = "Kill 50 zombies to win this achievement and some items",
 		["items"] = {{"Flashlight",1},{"Bandage",3}},
