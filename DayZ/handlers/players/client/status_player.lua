05146ac592edfaec79104db4bd46ec26294be2d2
mtadayz <rysttimes@gmail.com>
2017-04-14 17:55:24 +0200

    Added setting for camera sway when in pain (cfgClient)

--- DayZ/handlers/players/client/status_player.lua
+++ DayZ/handlers/players/client/status_player.lua
@@ -127,17 +127,31 @@ end
 addEvent("isPlayerInBuilding",true)
 addEventHandler("isPlayerInBuilding",root,isPlayerInBuilding)
 
+local painTimer = 1500
 function setPlayerPain()
 	if getElementData(localPlayer,"logedin") then
 		if getElementData(localPlayer,"pain") then
-			setCameraShakeLevel(gameplayVariables["painshakelevel"])
-			setTimer(setCameraShakeLevel,15000,1,0)
+			if gameplayVariables["painshakesway"] then
+				painTimer = 90000
+				setCameraShakeLevel(gameplayVariables["painshakelevel"])
+				setTimer(setCameraShakeLevel,15000,1,0)
+			else
+				painTimer = 1500
+				local x,y,z = getElementPosition(getLocalPlayer())
+				createExplosion (x,y,z+15,8,false,1.0,false)
+				local x, y, z, lx, ly, lz = getCameraMatrix() -- Get the current location and lookat of camera
+				x, lx = x + 1, lx + 1 -- What will be the new x and x lookat values
+				setCameraMatrix(x,y,z,lx,ly,lz) -- Set camera to new position
+				setCameraTarget (getLocalPlayer())
+			end
 		else
-			setCameraShakeLevel(0)
+			if gameplayVariables["painshakesway"] then
+				setCameraShakeLevel(0)
+			end
 		end
 	end
 end
-setTimer(setPlayerPain,90000,0)
+setTimer(setPlayerPain,painTimer,0)
 --[[ 
 Volume (Noise):
 
