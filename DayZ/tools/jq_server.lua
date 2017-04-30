1a3a669701d358ed5ee7d62ca691da43c85f0049
mtadayz <rysttimes@gmail.com>
2017-04-30 19:20:59 +0200

    no message

--- DayZ/tools/jq_server.lua
+++
@@ -1,7 +0,0 @@
-local root = getRootElement()
- 
-addEventHandler("onPlayerLogin", root,
-	function()
-		triggerClientEvent ( "onRollMessageStart", getRootElement(), getPlayerName(source).." #FFFFFFhas logged in!",255,255,255, "join")
-	end
-)
